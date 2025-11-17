09cb6c343cb1d9cc5ce65a188757f5d2a5b7692b
Mihir Tale <tale@usc.edu>
2025-11-16 17:28:00 -0800

    Added reranker

--- src/retrieval/__init__.py
+++ src/retrieval/__init__.py
@@ -5,8 +5,12 @@ Handles semantic search and tool retrieval from FAISS indexes.
 
 from .dense_retriever import ToolRetriever
 from .retrieval_metrics import RetrievalMetrics
+from .bm25_retriever import BM25Retriever
+from .bm25_plus_dense_retriever import HybridRetriever
 
 __all__ = [
     'ToolRetriever',
-    'RetrievalMetrics'
+    'RetrievalMetrics',
+    'BM25Retriever',
+    'HybridRetriever'
 ]
